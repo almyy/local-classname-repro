a20da3f73d7e56277f62262e5cb5edbbbf744ab5
Martin Almvik <martinalmvik@gmail.com>
2019-04-26 15:27:42 +0200

    Add repro project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import nonScoped from './NonModule.scss'
+import scoped from './Module.module.scss'
+
+console.log(nonScoped);
+// {}
+
+console.log(scoped);
+// {moduleStyle: "Module_moduleStyle__rQ__0"}
+
+const App: React.FC = () => (
+    <div>
+        <div className="global-style">global (works)</div>
+        <div className="global-from-module">global from module (works)</div>
+        <div className={scoped.moduleStyle}>module (works)</div>
+        <div className={nonScoped.localizedStyle}>localized (broken)</div>
     </div>
-  );
-}
+);
 
 export default App;
