89a12b489e075cb65439ecac53f885d47e0f4e83
Martin Almvik <martinalmvik@gmail.com>
2019-04-26 15:33:00 +0200

    Updates

---
+++ precache-manifest.7931442238d347fe9f9d249728e6d191.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d827ed23821a573ad87914d067496df6",
+    "url": "/local-classname-repro/index.html"
+  },
+  {
+    "revision": "c2e573d2d8d19a7abd0b",
+    "url": "/local-classname-repro/static/css/main.6f292d14.chunk.css"
+  },
+  {
+    "revision": "0b8c96f9de777eef4f60",
+    "url": "/local-classname-repro/static/js/2.5ecca776.chunk.js"
+  },
+  {
+    "revision": "c2e573d2d8d19a7abd0b",
+    "url": "/local-classname-repro/static/js/main.a959e864.chunk.js"
+  },
+  {
+    "revision": "daf14679644b06b6ccbb",
+    "url": "/local-classname-repro/static/js/runtime~main.5a6aadbc.js"
+  }
+]);
